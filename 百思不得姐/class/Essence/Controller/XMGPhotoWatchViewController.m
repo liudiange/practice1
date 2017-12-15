9b73199945498550d8478bdbe9096e97cac49199
liudiange <shaoyeliudiange@163.com>
2017-12-15 10:47:03 +0800

    保存到自定义相册方法抽取

--- 百思不得姐/class/Essence/Controller/XMGPhotoWatchViewController.m
+++ 百思不得姐/class/Essence/Controller/XMGPhotoWatchViewController.m
@@ -9,7 +9,6 @@
 #import "XMGPhotoWatchViewController.h"
 #import "DALabeledCircularProgressView.h"
 #import <Photos/Photos.h>
-#import <objc/message.h>
 
 @interface XMGPhotoWatchViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *saveButton;
@@ -44,8 +43,6 @@ static NSString *collectionTitle = @"百思不得姐的相簿";
     self.progressView.progressLabel.textColor = [UIColor redColor];
     self.progressView.progressTintColor = [UIColor greenColor];
     self.progressView.progressLabel.font = [UIFont systemFontOfSize:12.0];
-    
-    class_addMethod 
     
 }
 - (void)viewDidLoad {
@@ -131,46 +128,45 @@ static NSString *collectionTitle = @"百思不得姐的相簿";
  */
 - (void)saveImage {
     
-    __block PHAssetCollection *collection = nil;
-    PHFetchResult<PHAssetCollection *> *results =  [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-    for (PHAssetCollection *getCollection in results) {
-        if ([getCollection.localizedTitle isEqualToString:collectionTitle]) {
-            collection = getCollection;
-        }
-    }
     __block NSString *assetIdentifier = nil;
-    __block NSString *collectionIdentifier = nil;
     [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
         // 1.将图片保存到相簿
         assetIdentifier = [PHAssetCreationRequest creationRequestForAssetFromImage:self.downloadImage].placeholderForCreatedAsset.localIdentifier;
     } completionHandler:^(BOOL success, NSError * _Nullable error) {
         if (success) {
-            // 2.创建自定义的相薄
             [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-                if (!collection) {
-                  collectionIdentifier =  [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:collectionTitle].placeholderForCreatedAssetCollection.localIdentifier;
-                }
+                // 3. 将相片保存到自定义的相薄
+                PHAssetCollection *collection = [self getCollection];
+                PHAsset *asset = [PHAsset fetchAssetsWithLocalIdentifiers:@[assetIdentifier] options:nil].lastObject;
+                PHAssetCollectionChangeRequest *requestCollection = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
+                // 添加进自定义的相册
+                [requestCollection addAssets:@[asset]];
             } completionHandler:^(BOOL success, NSError * _Nullable error) {
                 if (success) {
-                    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-                        // 3. 将相片保存到自定义的相薄
-                        if (!collection) {
-                           collection = [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[collectionIdentifier] options:nil].lastObject;
-                        }
-                        PHAsset *asset = [PHAsset fetchAssetsWithLocalIdentifiers:@[assetIdentifier] options:nil].lastObject;
-                        PHAssetCollectionChangeRequest *requestCollection = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection];
-                        // 添加进自定义的相册
-                        [requestCollection addAssets:@[asset]];
-                    } completionHandler:^(BOOL success, NSError * _Nullable error) {
-                        if (success) {
-                            XMGLOG(@"保存相册成功了 哈哈哈");
-                        }
-                    }];
+                    XMGLOG(@"保存相册成功了 哈哈哈");
                 }
             }];
         }
     }];
 }
-
-
+/**
+ *
+ *  获取相薄
+ */
+- (PHAssetCollection *)getCollection {
+    
+    // 获取原来存在的collection
+     __block NSString *collectionIdentifier = nil;
+    PHFetchResult<PHAssetCollection *> *results =  [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    for (PHAssetCollection *getCollection in results) {
+        if ([getCollection.localizedTitle isEqualToString:collectionTitle]) {
+            return getCollection;
+        }
+    }
+   // 创建原来的collection 这个方法是同步执行的
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+          collectionIdentifier =  [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:collectionTitle].placeholderForCreatedAssetCollection.localIdentifier;
+    } error:nil];
+     return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[collectionIdentifier] options:nil].lastObject;
+}
 @end
