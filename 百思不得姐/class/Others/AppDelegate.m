7e976deee4ddb6a9c070bc56458512a1f676a538
liudiange <shaoyeliudiange@163.com>
2017-12-27 15:12:29 +0800

    点击状态栏 回到顶部 ，现在ios11 已经不用了

--- 百思不得姐/class/Others/AppDelegate.m
+++ 百思不得姐/class/Others/AppDelegate.m
@@ -13,7 +13,10 @@
 
 
 @interface AppDelegate ()
-
+{
+    UIWindow *window_;
+    
+}
 
 
 @end
@@ -31,8 +34,50 @@
     XMGTabBarController *tabBarVc = [[XMGTabBarController alloc]init];
     self.window.rootViewController = tabBarVc;
     [self.window makeKeyAndVisible];
-    
     return YES;
+}
+/**
+ *
+ *  点击状态栏，tableview滚动到头部
+ */
+- (void)addClickScrollerTop{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        //创建_window
+        window_ = [[UIWindow alloc] init];
+        window_.frame = CGRectMake(0, 0, ScreenWidth, 20);
+        window_.windowLevel = UIWindowLevelAlert;
+        window_.backgroundColor = [UIColor redColor];
+        // 直接显示了就
+        window_.hidden = NO;
+        //  添加点击手势
+        UITapGestureRecognizer *tapGes = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollerTap)];
+        [window_ addGestureRecognizer:tapGes];
+    });
+}
+/**
+ *
+ *  便利控制器，获取scrollerview 进行滚动到顶部
+ */
+- (void)scrollerTap {
+    
+    [self getScrollerView:[UIApplication sharedApplication].keyWindow];
+    
+    
+}
+/**
+ *
+ *  获取scroller
+ */
+- (void)getScrollerView:(UIView *)view {
+    
+    for (UIView *subView in view.subviews) {
+        [self getScrollerView:subView];
+    }
+    if (![view isKindOfClass:[UIScrollView class]]) return;
+    UIScrollView *scrollerView = (UIScrollView *)view;
+    CGPoint point = scrollerView.contentOffset;
+    point.y = - scrollerView.contentInset.top;
+    [scrollerView setContentOffset:point animated:YES];
 }
 /**
  *
