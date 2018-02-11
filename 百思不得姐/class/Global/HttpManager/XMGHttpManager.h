772b1e9e4d0b9658cf50eb04be7bd260620db045
shaoyeliudiange <shaoyeliudiange@163.com>
2018-02-11 10:51:15 +0800

    小小的修改

--- 百思不得姐/class/Global/HttpManager/XMGHttpManager.h
+++ 百思不得姐/class/Global/HttpManager/XMGHttpManager.h
@@ -21,6 +21,8 @@
 @property (nonatomic, strong) AFHTTPSessionManager *manager;
 // 一个小小的测试
 @property (nonatomic, strong) NSString *str;
+// 一个小小的测试
+@property (nonatomic, strong) NSString *ashdhashd;
 /**
  请求的任务
  */
